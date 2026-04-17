6a53b29ff7db4c77133974d40bacb9030b5fbaea
Tom Meehan <tom.meehan@me.com>
2026-04-17 14:24:32 +0100

    fix: temporarily remove harper_grace from Klaviyo silver models (TRANSFORMER grants pending)

--- dbt/models/silver/stg_klaviyo__events.sql
+++ dbt/models/silver/stg_klaviyo__events.sql
@@ -34,30 +34,10 @@ deese_pro as (
        or array_size(_airbyte_meta:changes) = 0
 ),
 
-harper_grace as (
-    select
-        id::varchar                                                 as event_id,
-        type::varchar                                               as event_type,
-        datetime::timestamp_tz                                      as occurred_at,
-        attributes:value::float                                     as value,
-        attributes:value_currency::varchar                          as value_currency,
-        attributes:unique_id::varchar                               as unique_id,
-        attributes:event_properties:"$campaign_id"::varchar         as campaign_id,
-        attributes:event_properties:"$flow_id"::varchar             as flow_id,
-        attributes:event_properties:"$message_id"::varchar          as message_id,
-        relationships:profile:data:id::varchar                      as profile_id,
-        'harper_grace'                                              as store_id
-    from {{ source('bronze_klaviyo_harper_grace', 'events') }}
-    where _airbyte_meta:changes is null
-       or array_size(_airbyte_meta:changes) = 0
-),
-
 unioned as (
     select * from isclinical
     union all
     select * from deese_pro
-    union all
-    select * from harper_grace
 )
 
 select * from unioned
